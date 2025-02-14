bc9573a089e4360bc754f265c6680bcad7dec0d9
M3IK3DEV <130098498+MikeDEV2022@users.noreply.github.com>
2025-02-14 00:36:10 -0300

    v1.1
    Atualizei o javascript implementando o sistema de horas, e corrigi (50%) um problema de estrutura de altura da pagina

--- scripts/home.js
+++ scripts/home.js
@@ -1,14 +1,22 @@
-const HORAS = new Date();
 document.getElementById("data-input").addEventListener("click",function(event){
     event.preventDefault();
+    const HORAS = new Date();
     let newPost = document.createElement("div");
     let campText = document.getElementById("tx1").value
+    let horas = HORAS.getHours();
+    let minutos = HORAS.getMinutes();
+    if(minutos < 10){
+        minutos = `0${minutos}`
+    }
+    if(horas < 10){
+        horas = `0${horas}`
+    }
     newPost.innerHTML = `<div class="comentary-card">
                 <div class="user-comentary">
                     <img src="/content/icon-user.png" alt="user">
                     <div class="user-informations-comentary">
                         <h1>Mario Oliveira</h1>
-                        <p>15h 33m</p>
+                        <p>${horas}h ${minutos}m</p>
                     </div>
                 </div>
                 <div class="text-response">
